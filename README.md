d957fc1523d65b148517a3f7d0a53a80f123508c
Eliseo Avila <ebae82@hotmail.com>
2022-10-07 21:01:31 -0300

    Cargando proyecto en repositorio remoto

---
+++ README.md
@@ -0,0 +1,39 @@
+# Aplicación generadora de consejos
+
+El desafío es desarrollar esta aplicación generadora de consejos y lograr que se parezca lo más posible al diseño.
+
+- HTML
+- CSS
+- JS Vanilla (with animejs library)
+
+# Guía de Estilo
+
+## Diseño
+
+Los diseños se crearon con los siguientes anchos:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colores
+### Primarios
+
+- Light Cyan: hsl(193, 38%, 86%)
+- Neon Green: hsl(150, 100%, 66%)
+
+### Neutrales
+
+- Grayish Blue: hsl(217, 19%, 38%)
+- Dark Grayish Blue: hsl(217, 19%, 24%)
+- Dark Blue: hsl(218, 23%, 16%)
+
+## Tipografía
+
+### Cuerpo
+
+- Font size (quote): 28px
+
+### Fuentes
+
+- Family: [Manrope](https://fonts.google.com/specimen/Manrope)
+- Weights: 800
---
+++ main.js
@@ -0,0 +1,89 @@
+const button = document.getElementById('dice')
+const advice = document.getElementById('advice')
+const adviceNumberTitle = document.getElementById('advice_number')
+const maxNumberAdvice = 224
+let adviceNumber = 71
+
+// We write in the DOM the number of advice
+adviceNumberTitle.innerHTML = `ADVICE #${adviceNumber}`
+
+// Function that generates a random number
+button.addEventListener('click', () => {
+    // We generate a new number of advice
+    adviceNumber = Math.floor(Math.random() * maxNumberAdvice)
+    // We check that the number is greater than zero
+    if (adviceNumber > 0) {
+        setTimeout(() => {
+            // // We write in the DOM the new number of advice
+            adviceNumberTitle.innerHTML = `ADVICE #${adviceNumber}`
+        }, 500);
+        // We run the function
+        runApi()
+
+    } else {
+        advice.innerHTML = 'Advice not found, please try again'
+    }
+})
+
+// Function to consume the API
+const runApi = async () => {
+    try {
+        const URL = `https://api.adviceslip.com/advice/${adviceNumber}`
+        const response = await fetch(URL)
+        // We check the status of the response
+        if (response.status === 200) {
+            const data = await response.json()
+            const dataAdvice = data.slip.advice
+            // We write the advice in the DOM
+            advice.innerHTML = `"${dataAdvice}"`
+        } else if (response.status === 400) {
+            console.log('The searched content is not found')
+        } else {
+            console.log('Unidentified error')
+        }
+        // Animations
+        anime({
+            targets: '.container',
+            scale: [0, 1],
+            opacity: {
+                value: [0, 1],
+                duration: 1000
+            },
+            rotate: {
+                value:[-360, 360],
+                duration: 500,
+                easing: 'easeInOutSine'
+            },
+            duration: 200,
+            easing: 'easeInOutQuart'
+        });
+
+        anime({
+            targets: '.card_dice',
+            rotate: {
+                value:[-360, 360],
+                duration: 500,
+                delay: 100,
+                easing: 'easeInOutSine'
+            },
+            scale: {
+                value: [0, 1],
+                delay: 100,
+                duration: 200,
+                easing: 'easeInOutSine'
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+runApi()
+
+
+
+
+
+
+
